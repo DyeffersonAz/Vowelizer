2ef700d11f35df53be80d7256e4b27e3f73607a8
DyeffersonAz <dyeffersonazevedo@hotmail.com>
2019-05-19 15:02:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vowelizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write the sentence you want to vowelize: ");
+            string song = Console.ReadLine();
+
+            Console.Write("What is the vowel you want? ");
+            string vowel = Console.ReadLine();
+
+            song = song.Replace("a", vowel);
+            song = song.Replace("á", vowel);
+            song = song.Replace("â", vowel);
+            song = song.Replace("ã", vowel);
+            song = song.Replace("à", vowel);
+            song = song.Replace("e", vowel);
+            song = song.Replace("é", vowel);
+            song = song.Replace("ê", vowel);
+            song = song.Replace("i", vowel);
+            song = song.Replace("í", vowel);
+            song = song.Replace("î", vowel);
+            song = song.Replace("o", vowel);
+            song = song.Replace("ó", vowel);
+            song = song.Replace("ô", vowel);
+            song = song.Replace("u", vowel);
+            song = song.Replace("ú", vowel);
+            song = song.Replace("û", vowel);
+            
+            Console.WriteLine(song);
+        }
+    }
+}
