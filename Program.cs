fb7d4aba192ef3417e48d055e2bba210d41db5f0
Lucas Nuic <165694895+lucasnuic@users.noreply.github.com>
2024-05-01 15:05:38 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,29 +8,17 @@ namespace Vowelizer
         {
             Console.Write("Write the sentence you want to vowelize: ");
             string song = Console.ReadLine();
-
             Console.Write("What is the vowel you want? ");
             string vowel = Console.ReadLine();
-
-            song = song.Replace("a", vowel);
-            song = song.Replace("á", vowel);
-            song = song.Replace("â", vowel);
-            song = song.Replace("ã", vowel);
-            song = song.Replace("à", vowel);
-            song = song.Replace("e", vowel);
-            song = song.Replace("é", vowel);
-            song = song.Replace("ê", vowel);
-            song = song.Replace("i", vowel);
-            song = song.Replace("í", vowel);
-            song = song.Replace("î", vowel);
-            song = song.Replace("o", vowel);
-            song = song.Replace("ó", vowel);
-            song = song.Replace("ô", vowel);
-            song = song.Replace("u", vowel);
-            song = song.Replace("ú", vowel);
-            song = song.Replace("û", vowel);
+            string[] characters = { "a", "á", "â", "ã", "à", "e", "é", "ê", "i", "í", "î", "o", "ó", "ô", "u", "ú", "û" };
+             
+            foreach (string character in characters)
+            {
+                 Console.WriteLine(song.Replace(character, vowel));
+            }   
             
             Console.WriteLine(song);
         }
     }
 }
+ 
